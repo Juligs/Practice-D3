898259cdb962762b373f819ca02cf1db2faa22a7
Juliana Gomez <julicodes29@gmail.com>
2023-03-30 12:39:59 +0200

    transition

--- archivos_base/06 - Animación e interacción/01 - Eventos/app_inicio.js
+++ archivos_base/06 - Animación e interacción/01 - Eventos/app_inicio.js
@@ -1,59 +1,59 @@
-var datos = [144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+let datos = [
+  144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+];
 
 function graficar() {
-    var w = 500;
-    var h = 300;
+  let w = 500;
+  let h = 300;
 
-    var svg = d3.select('body')
-        .append('svg')
-        .attr("width", w)
-        .attr("height", h);
+  let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-    svg.selectAll("rect")
-        .data(datos)
-        .enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 20)
-        .attr("height", 100)
+  svg
+    .selectAll("rect")
+    .data(datos)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 20)
+    .attr("height", 100)
 
-    .attr("x", function(d, i) {
-        return i * 21 + 30;
+    .attr("x", function (d, i) {
+      return i * 21 + 30;
     })
 
-    .attr("height", function(d) {
-        return d + 50;
+    .attr("height", function (d) {
+      return d + 50;
     })
 
-    .attr("y", function(d) {
-            return h - d - 50;
-        })
+    .attr("y", function (d) {
+      return h - d - 50;
+    })
     .attr("fill", "SteelBlue")
 
-    .on("mouseover", function(){
-        d3.select(this)
-        .attr("fill", "tomato")
+    .on("mouseover", function () {
+      // Método on llama al evento, mouseover
+      d3.select(this).attr("fill", "tomato"); // this hace referencia al elemento que se está seleccionando cada uno de los elementos
+      //fill, color de relleno
     })
 
-    .on("mouseout", function(){
-        d3.select(this)
-        .attr("fill", "SteelBlue")
+    .on("mouseout", function () {
+      // Método on, mouseout deja el color original
+      d3.select(this).attr("fill", "SteelBlue");
+    });
+
+  svg
+    .selectAll("text")
+    .data(datos)
+    .enter()
+    .append("text")
+    .text(function (d) {
+      return d;
     })
-
-
-
-    svg.selectAll("text")
-        .data(datos)
-        .enter()
-        .append("text")
-        .text(function(d) {
-            return d;
-        })
-        .attr("x", function(d, i) {
-            return i * 21 + 40;
-        })
-        .attr("y", function(d) {
-            return h - d - 53;
-        })
+    .attr("x", function (d, i) {
+      return i * 21 + 40;
+    })
+    .attr("y", function (d) {
+      return h - d - 53;
+    });
 }
--- archivos_base/06 - Animación e interacción/02 - Transiciones/app_inicio.js
+++ archivos_base/06 - Animación e interacción/02 - Transiciones/app_inicio.js
@@ -1,60 +1,66 @@
-var datos = [144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+let datos = [
+  144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+];
 
 function graficar() {
-    var w = 500;
-    var h = 300;
+  let w = 500;
+  let h = 300;
 
-    var svg = d3.select('body')
-        .append('svg')
-        .attr("width", w)
-        .attr("height", h);
+  let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-    svg.selectAll("rect")
-        .data(datos)
-        .enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 20)
-        .attr("height", 100)
+  svg
+    .selectAll("rect")
+    .data(datos)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 0)
+    .attr("height", 100)
 
-    .attr("x", function(d, i) {
-        return i * 21 + 30;
+    .attr("x", function (d, i) {
+      return i * 21 + 30;
     })
 
-    .attr("height", function(d) {
-        return d + 50;
+    .attr("height", function (d) {
+      return d + 50;
     })
 
-    .attr("y", function(d) {
-            return h - d - 50;
-        })
-        .attr("fill", "SteelBlue")
+    .attr("y", function (d) {
+      return h - d - 50;
+    })
+    .attr("fill", "SteelBlue")
 
+    .on("mouseover", function () {
+      d3.select(this).attr("fill", "tomato");
+    })
 
-    .on("mouseover", function() {
-            d3.select(this)
-                .attr("fill", "tomato");
-        })
-    
-    .on("mouseout", function(d, i) {
-            d3.select(this)
-                .attr("fill", "SteelBlue");
-        })
-  
+    .on("mouseout", function (d, i) {
+      d3.select(this).attr("fill", "SteelBlue");
+    })
 
+    .transition()
+    .delay(function (d, i) {
+      //Cuantos milisegundos tarda en ejecutar la animacion, seguin el orden de los datos
+      return i * 100; //si quito el * son todos a la vez
+    })
+    .duration(2000)
+    .attr("width", function (d) {
+      return 20;
+    });
 
-    svg.selectAll("text")
-        .data(datos)
-        .enter()
-        .append("text")
-        .text(function(d) {
-            return d;
-        })
-        .attr("x", function(d, i) {
-            return i * 21 + 40;
-        })
-        .attr("y", function(d) {
-            return h - d - 53;
-        })
+  svg
+    .selectAll("text")
+    .data(datos)
+    .enter()
+    .append("text")
+    .text(function (d) {
+      return d;
+    })
+    .attr("x", function (d, i) {
+      return i * 21 + 40;
+    })
+    .attr("y", function (d) {
+      return h - d - 53;
+    });
 }
